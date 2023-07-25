ff32333557d8e0b23e29e7477fbc8ba61b7966f6
wumengyan <mengyanw@umich.edu>
2023-07-25 10:06:09 -0400

    init extension from template

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import { Token } from '@lumino/coreutils';
+
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { requestAPI } from './handler';
+
+const PLUGIN_ID = 'telemetry-router:plugin';
+
+export const ITelemetryRouter = new Token<ITelemetryRouter>(PLUGIN_ID)
+
+export interface ITelemetryRouter {
+  hi(): any;
+}
+
+class telemetryRouter implements ITelemetryRouter {
+  hi(): any {
+    console.log("*************HELLO FROM TELEMETRY ROUTER***************")
+  }
+}
+/**
+ * Initialization data for the telemetry-router extension.
+ */
+const plugin: JupyterFrontEndPlugin<telemetryRouter> = {
+  id: PLUGIN_ID,
+  description: 'A JupyterLab extension.',
+  provides: ITelemetryRouter,
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension telemetry-router is activated!')
+    requestAPI<any>('get-example')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(reason => {
+        console.error(
+          `The telemetry_router server extension appears to be missing.\n${reason}`
+        );
+      });
+    const _telemetryRouter = new telemetryRouter()
+    console.log('router side test: ')
+    _telemetryRouter.hi()
+    return _telemetryRouter;
+  }
+};
+
+export default plugin;
