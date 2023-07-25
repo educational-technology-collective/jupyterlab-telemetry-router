a5a470320aa89d10e8393a5bad9a6f1fb9100728
wumengyan <mengyanw@umich.edu>
2023-07-25 16:28:03 -0400

    serialize notebook content & clean up code

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,60 @@
-import { UUID, Token } from '@lumino/coreutils';
-
 import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin,
-  // LabShell
 } from '@jupyterlab/application';
 
-import { requestAPI } from './handler';
+// import { requestAPI } from './handler';
 
-// import { INotebookContent } from '@jupyterlab/nbformat';
+import { NotebookPanel } from '@jupyterlab/notebook';
 
-import {
-  NotebookPanel,
-  // Notebook
-} from '@jupyterlab/notebook';
+import { INotebookContent } from '@jupyterlab/nbformat';
+
+import { Token } from '@lumino/coreutils';
 
 const PLUGIN_ID = 'telemetry-router:plugin';
 
 export const ITelemetryRouter = new Token<ITelemetryRouter>(PLUGIN_ID)
 
 export interface ITelemetryRouter {
-  loadNotebook(notebookPanel: NotebookPanel): void;
+  loadNotebookPanel(notebookPanel: NotebookPanel): void;
   consumeEventSignal(data: Object): void;
 }
 
 export class TelemetryRouter implements ITelemetryRouter {
-  private session_id: string;
-  private seq: number;
+  private session_id?: string;
+  private sequence: number;
   private notebookPanel?: NotebookPanel;
 
   constructor() {
-    this.session_id = UUID.uuid4();
-    this.seq = 0;
+    this.sequence = 0;
   }
 
-  loadNotebook(notebookPanel: NotebookPanel) {
+  loadNotebookPanel(notebookPanel: NotebookPanel) {
     this.notebookPanel = notebookPanel
   }
 
-  consumeEventSignal(event: Object): any {
-    console.log("router received event signal", event)
+  consumeEventSignal(event: Object) {
+    // Check if session id received is equal to the stored session id &
+    // Update sequence number accordingly
+    if (this.session_id === this.notebookPanel?.sessionContext.session?.id)
+      this.sequence = this.sequence + 1
+    else {
+      this.session_id = this.notebookPanel?.sessionContext.session?.id
+      this.sequence = 0
+    }
 
-    const data = {
+    // Construct log
+    const log = {
       event: event,
       notebookState: {
         session_id: this.session_id,
-        seq: this.seq,
-        notebookPanel: this.notebookPanel
+        sequence: this.sequence,
+        notebookContent: this.notebookPanel?.model?.toJSON() as INotebookContent
       },
     }
 
-    // prepare sequence number for NEXT inquiry
-    this.seq = this.seq + 1;
-
-    console.log("router integrated data", data)
+    // Post to database
+    console.log("router log", log)
   }
 }
 
@@ -64,15 +65,6 @@ const plugin: JupyterFrontEndPlugin<TelemetryRouter> = {
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
     console.log('JupyterLab extension telemetry-router is activated!')
-    requestAPI<any>('get-example')
-      .then(data => {
-        console.log(data);
-      })
-      .catch(reason => {
-        console.error(
-          `The telemetry_router server extension appears to be missing.\n${reason}`
-        );
-      });
 
     const telemetryRouter = new TelemetryRouter()
     return telemetryRouter;
