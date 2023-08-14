71a910b73c147aa541da76b670808889d5340993
Mengyan Wu <mengyanw@umich.edu>
2023-08-14 10:05:09 -0400

    feat: otel router basic

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,42 @@ import { INotebookContent } from '@jupyterlab/nbformat';
 
 import { Token } from '@lumino/coreutils';
 
-import {
-  // Consumer,
-  // ConsoleLogger,
-  // MongoDBLogger,
-  // S3Logger,
-  // InfluxDBLogger,
-  ConsumerCollection,
-} from './consumer';
+// import {
+//   // Consumer,
+//   // ConsoleLogger,
+//   // MongoDBLogger,
+//   // S3Logger,
+//   // InfluxDBLogger,
+//   ConsumerCollection,
+// } from './consumer';
 
 import { requestAPI } from './handler';
+
+import {
+  trace,
+  // context,
+  SpanStatusCode
+} from "@opentelemetry/api";
+
+import { registerTracerProvider } from './otel';
+
+registerTracerProvider();
+const tracer = trace.getTracer('jupyter-opentelemetry'); // Returns a tracer from the **global** tracer provider.
+
+// let rootSpan: any;
+const spanContextInjector = (data: any) => {
+  // const ctx = trace.setSpan(context.active(), rootSpan);
+  // const span = tracer.startSpan(data.event.eventName, undefined, ctx)
+  if (data.event.eventName) { }
+  const span = tracer.startSpan(data.event.eventName)
+
+  span.setStatus({
+    code: SpanStatusCode.OK,
+    message: 'OK'
+  })
+
+  span.end()
+}
 
 const PLUGIN_ID = 'telemetry-router:plugin';
 
@@ -30,8 +56,8 @@ export interface ITelemetryRouter {
 }
 
 export class TelemetryRouter implements ITelemetryRouter {
-  private sessionID?: string;
-  private sequence: number = 0;
+  // private sessionID?: string;
+  // private sequence: number = 0;
   private notebookPanel?: NotebookPanel;
 
   loadNotebookPanel(notebookPanel: NotebookPanel) {
@@ -39,37 +65,39 @@ export class TelemetryRouter implements ITelemetryRouter {
   }
 
   async publishEvent(event: Object) {
-    // Check if session id received is equal to the stored session id &
-    // Update sequence number accordingly
-    if (this.sessionID && this.sessionID === this.notebookPanel?.sessionContext.session?.id) {
-      this.sequence = this.sequence + 1
-    }
-    else {
-      this.sessionID = this.notebookPanel?.sessionContext.session?.id
-      this.sequence = 0
-    }
+    // // Check if session id received is equal to the stored session id &
+    // // Update sequence number accordingly
+    // if (this.sessionID && this.sessionID === this.notebookPanel?.sessionContext.session?.id) {
+    //   this.sequence = this.sequence + 1
+    // }
+    // else {
+    //   this.sessionID = this.notebookPanel?.sessionContext.session?.id
+    //   this.sequence = 0
+    // }
 
     // Get environment data
     const { workspaceID } = await requestAPI<any>('env')
 
-    // Construct log
-    const log = {
+    // Construct data
+    const data = {
       event: event,
       notebookState: {
         workspaceID: workspaceID,
-        sessionID: this.sessionID,
-        sequence: this.sequence,
+        // sessionID: this.sessionID,
+        // sequence: this.sequence,
         notebookPath: this.notebookPanel?.context.path,
         notebookContent: this.notebookPanel?.model?.toJSON() as INotebookContent
       },
     }
 
-    // Send to consumer
-    ConsumerCollection.forEach(consumer => {
-      if (consumer.id == 'ConsoleLogger') {
-        new consumer().consume(log);
-      }
-    });
+    spanContextInjector(data)
+
+    // // Send to consumer
+    // ConsumerCollection.forEach(consumer => {
+    //   if (consumer.id == 'ConsoleLogger') {
+    //     new consumer().consume(log);
+    //   }
+    // });
   }
 }
 
