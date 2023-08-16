4a11bfa09bac7259bb5211b6b881d48add522ffe
Mengyan Wu <mengyanw@umich.edu>
2023-08-16 10:17:16 -0400

    chore: cleanup and add jsdoc

--- src/index.ts
+++ src/index.ts
@@ -25,11 +25,22 @@ export class TelemetryRouter implements ITelemetryRouter {
   private notebookPanel?: NotebookPanel;
   private workspaceID?: string;
 
+  /**
+   * Load notebookPanel and get workspaceID.
+   * 
+   * @param {NotebookPanel} notebookPanel 
+   */
   async loadNotebookPanel(notebookPanel: NotebookPanel) {
     this.notebookPanel = notebookPanel
     this.workspaceID = await requestAPI<any>('env')
   }
 
+  /**
+   * Send event data to consumers defined in the configuration file.
+   * 
+   * @param {Object} eventDetail An object containing event details 
+   * @param {Boolean} logNotebookContent A boolean indicating whether to log the entire notebook or not
+   */
   async publishEvent(eventDetail: Object, logNotebookContent?: Boolean) {
     // Check if session id received is equal to the stored session id
     if (!this.sessionID || this.sessionID !== this.notebookPanel?.sessionContext.session?.id) {
@@ -43,7 +54,7 @@ export class TelemetryRouter implements ITelemetryRouter {
         sessionID: this.sessionID,
         workspaceID: this.workspaceID,
         notebookPath: this.notebookPanel?.context.path,
-        notebookContent: logNotebookContent ? this.notebookPanel?.model?.toJSON() as INotebookContent : null
+        notebookContent: logNotebookContent ? this.notebookPanel?.model?.toJSON() as INotebookContent : null // decide whether to log the entire notebook
       },
     }
 
@@ -53,19 +64,25 @@ export class TelemetryRouter implements ITelemetryRouter {
   }
 }
 
+/** 
+ * Activate the extension
+ * 
+ * @param {JupyterFrontEnd} app - The JupyterLab application instance. 
+ * @return {Promise<TelemetryRouter>} - A promise that returns TelemetryRouter if resolved.
+*/
+async function activate(app: JupyterFrontEnd): Promise<TelemetryRouter> {
+  const version = await requestAPI<string>('version')
+  console.log(`${PLUGIN_ID}: ${version}`)
+  const telemetryRouter = new TelemetryRouter();
+  return telemetryRouter;
+}
+
 const plugin: JupyterFrontEndPlugin<TelemetryRouter> = {
   id: PLUGIN_ID,
   description: 'A JupyterLab extension.',
   provides: ITelemetryRouter,
   autoStart: true,
-  activate: async (app: JupyterFrontEnd) => {
-    const version = await requestAPI<string>('version')
-    console.log(`${PLUGIN_ID}: ${version}`)
-
-    const telemetryRouter = new TelemetryRouter();
-
-    return telemetryRouter;
-  }
+  activate: activate
 };
 
 export default plugin;
