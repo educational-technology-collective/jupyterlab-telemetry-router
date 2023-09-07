49bdbcfdf05151448243ff39dcb8eb24357cddba
Mengyan Wu <mengyanw@umich.edu>
2023-09-07 16:40:49 -0400

    chore: rename

--- jupyterlab_telemetry_router/__init__.py
+++ jupyterlab_telemetry_router/__init__.py
@@ -5,7 +5,7 @@ except ImportError:
     # in editable mode with pip. It is highly recommended to install
     # the package from a stable release or in editable mode: https://pip.pypa.io/en/stable/topics/local-project-installs/#editable-installs
     import warnings
-    warnings.warn("Importing 'jupyterlab-telemetry-router' outside a proper installation.")
+    warnings.warn("Importing 'jupyterlab_telemetry_router' outside a proper installation.")
     __version__ = "dev"
 
 from .application import JupyterLabTelemetryRouterApp
@@ -18,7 +18,7 @@ def _jupyter_labextension_paths():
 
 def _jupyter_server_extension_points():
     return [{
-        "module": "jupyterlab-telemetry-router",
+        "module": "jupyterlab_telemetry_router",
         "app": JupyterLabTelemetryRouterApp
     }]
 
